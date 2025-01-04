9e36db1cd1af5d9c7b4aff4e4f544b9391730175
Eugene Afriyie <groupeight00@gmail.com>
2025-01-04 12:19:32 +0000

    created the tab ccomponent

--- Beginner Projects/First_project/src/Component/Profile.tsx
+++ Beginner Projects/First_project/src/Component/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { FaCamera } from 'react-icons/fa';
+import Tab from './Tab';
 
 const Profile = () => {
     const [bannerUrl,setBannerurl] = useState('https://dummyimage.com/1500x400');
@@ -14,6 +15,14 @@ const Profile = () => {
             setBannerurl(URL.createObjectURL(file))
         }
     } 
+
+    const handleProfileChange = (event:any) =>{
+        const file = event.target.files[0];
+
+        if (file){
+            setProfileUrl(URL.createObjectURL(file))
+        }
+    }
 
   return (
     <div className='relative w-[94%] ml-[5rem]'>
@@ -39,22 +48,44 @@ const Profile = () => {
 
         {/* Channel Logo  */}
 
-        <div className="flex items-center m-4  mt-[2rem] border border-red-500">
+        <div className="flex items-center m-4  mt-[2rem] border border-red-500 relative">
+                    <div className="flex items-center justify-center m-4 mt-[2rem] border border-red-500 relative">
             <img 
                 src={profileUrl} 
-                alt="prolile img"
-                className='w-40 h-40 0bject-cover rounded-full border-white relative'
-              />
-
-            <button className="absolute  -ml-[6rem] z-10 mt-[10rem] p-2 bg-gray-800 text-white rounded-full hover:bg-gray-600">
-                <label htmlFor="proflie-upload"
-                       className='cursor-pointer'>
-
-                <FaCamera size={24}/>
+                alt="profile img"
+                className="w-40 h-40 object-cover rounded-full border-white"
+            />
+            <button 
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2 bg-gray-800 text-white rounded-full hover:bg-gray-600"
+            >
+                <label htmlFor="profile-upload" className="cursor-pointer">
+                <FaCamera size={24} />
                 </label>
+
+                <input type="file" id='profile-upload' accept='image/*'className='hidden' onChange={handleProfileChange} />
+            </button>
+        </div>
+
+
+        {/* Channel details  */}
+
+        <div className="ml-4 mt-4 ">
+            <h1 className="text-2xl font-bold ml-[4remn]"> Eugene Afriyie</h1>
+            <p>1M  Views</p>
+            <p className="mt-2">
+                This is a short description of the YouTube Channel. It gives an overview of the content and purpose of the channel.
+            </p>
+
+            <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-rose-500">
+                Subscibe
             </button>
 
         </div>
+
+
+        </div>
+
+        <Tab/>
     </div>
   )
 }
