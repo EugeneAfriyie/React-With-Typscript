8f2828b995175f79b5d4a98d93feeead8ac9f2e0
Eugene Afriyie <groupeight00@gmail.com>
2025-01-05 20:38:08 +0000

    full code for the topics comp

--- Beginner Projects/Second-Project/src/App.tsx
+++ Beginner Projects/Second-Project/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigation from './Components/Navigation'
 import PeolpleToFollow from './Components/PeopleToFollow'
 import TrendsList from './Components/TrandList'
+import TopicsList from './Components/TopicsList'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <div className="w-[30%]">
           <PeolpleToFollow/>
           <TrendsList/>
+          <TopicsList/>
         </div>
       </div>
     </div>
