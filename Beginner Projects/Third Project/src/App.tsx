2621a97f12091a4a20e7213232dbe306fa11499b
Eugene Afriyie <groupeight00@gmail.com>
2025-01-13 23:20:31 +0000

    remove app content

--- Beginner Projects/Third Project/src/App.tsx
+++ Beginner Projects/Third Project/src/App.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="relative overflow-hidden h-10">
-      <div className="absolute whitespace-nowrap animate-scroll text-green-700">
-        Eugene Afriyie is my name
-      </div>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
