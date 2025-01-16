f8e377508b5b0f53608af569debf1c4294f56791
Eugene Afriyie <groupeight00@gmail.com>
2025-01-16 23:51:35 +0000

    sidebar content

--- Beginner Projects/Third Project/src/Components/Dashboard.tsx
+++ Beginner Projects/Third Project/src/Components/Dashboard.tsx
@@ -1,12 +1,11 @@
 import Sidebar from "./Sidebar";
-import ProjectTable from "./Table";
 
 const Dashboard = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 bg-gray-900">
-        <ProjectTable />
+        {/* <ProjectTable /> */}
       </div>
     </div>
   );
