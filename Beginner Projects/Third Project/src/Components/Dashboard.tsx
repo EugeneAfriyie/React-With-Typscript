f646381f2bf719cd0652fa4bce6f04c7ad915be2
Eugene Afriyie <groupeight00@gmail.com>
2025-01-18 21:24:56 +0000

    table tsx ffile content

--- Beginner Projects/Third Project/src/Components/Dashboard.tsx
+++ Beginner Projects/Third Project/src/Components/Dashboard.tsx
@@ -1,11 +1,12 @@
 import Sidebar from "./Sidebar";
+import ProjectTable from "./Table";
 
 const Dashboard = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 bg-gray-900">
-        {/* <ProjectTable /> */}
+        <ProjectTable />
       </div>
     </div>
   );
