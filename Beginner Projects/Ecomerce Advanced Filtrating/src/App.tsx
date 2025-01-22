a15aa162d590804107d7279d52d01ee163de888b
Eugene Afriyie <groupeight00@gmail.com>
2025-01-22 23:44:52 +0000

    started with app comp[onent

--- Beginner Projects/Ecomerce Advanced Filtrating/src/App.tsx
+++ Beginner Projects/Ecomerce Advanced Filtrating/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+        <div className="flex h-screen"></div>
+    </Router>
   )
 }
 
